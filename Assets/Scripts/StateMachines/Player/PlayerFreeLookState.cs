b3aa5e23caefcb2c6a921d4f7eab38232eb397bd
Aybars Duran <aybarsduran@outlook.com>
2023-05-08 02:51:20 +0300

    Smooth Rotation, Terrain bug

---
+++ Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerFreeLookState : PlayerBaseState
+{
+    private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed"); // animator int ile stringden daha hizli calistigi icin string to hash yaptýk ve readonly yaptýk
+    private const float AnimatorDampTime = 0.1f;
+    public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        
+    }
+    public override void Tick(float deltaTime)
+    {
+        Vector3 movement = CalculateMovement();
+
+        stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if (stateMachine.InputReader.MovementValue == Vector2.zero)
+        {
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, AnimatorDampTime, deltaTime);
+            return;
+        }
+
+        stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, AnimatorDampTime, deltaTime);
+
+        FaceMovementDirection(movement,deltaTime);
+
+    }
+
+   
+
+    public override void Exit()
+    {
+      
+
+
+    }
+    
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+    private Vector3 CalculateMovement()
+    {
+        Vector3 right = stateMachine.MainCameraTransform.right;
+        Vector3 forward= stateMachine.MainCameraTransform.forward;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y + right * stateMachine.InputReader.MovementValue.x;
+
+    }
+    private void FaceMovementDirection(Vector3 movement,float deltaTime)
+    {
+        stateMachine.transform.rotation = Quaternion.Lerp(stateMachine.transform.rotation, Quaternion.LookRotation(movement),
+            deltaTime * stateMachine.RotationDamping);
+    }
+
+
+}
--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -10,12 +10,15 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
+    [field: SerializeField] public float RotationDamping { get; private set; }
     [field: SerializeField] public Animator Animator{ get; private set; }
+    public Transform MainCameraTransform { get; private set; }
 
 
     private void Start()
     {
-        SwitchState(new PlayerTestState(this));
+        MainCameraTransform = Camera.main.transform;
+        SwitchState(new PlayerFreeLookState(this));
     }
 
 }
