2a79a46682a36f677d1f6899a850b9b490f7f1ef
Aybars Duran <aybarsduran@outlook.com>
2023-05-16 17:44:04 +0300

    Added MovementStateType script

---
+++ Assets/Controller/Movement/MovementStateType.cs
@@ -0,0 +1,18 @@
+namespace IdenticalStudios
+{
+    public enum MovementStateType
+    {
+        None = -1,
+        Idle = 0,
+        Walk = 10,
+        Run = 20,
+        Slide = 30,
+        Crouch = 40,
+        Jump = 50,
+        Airborne = 60,
+        LadderClimb = 70, // Not Implemented   
+        // Swim = 90,        // Not Implemented
+
+       
+    }
+}
