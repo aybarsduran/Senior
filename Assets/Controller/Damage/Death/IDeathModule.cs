b89f77b20a383f020c1c475a823fa76cee4cdb31
Aybars Duran <aybarsduran@outlook.com>
2023-05-18 15:30:37 +0300

    added IDeathModule interface

---
+++ Assets/Controller/Damage/Death/IDeathModule.cs
@@ -0,0 +1,10 @@
+using IdenticalStudios;
+
+namespace IdenticalStudios
+{
+    public interface IDeathModule : ICharacterModule
+    {
+        void DoDeathEffects(ICharacter character);
+        void DoRespawnEffects(ICharacter character);
+    }
+}
