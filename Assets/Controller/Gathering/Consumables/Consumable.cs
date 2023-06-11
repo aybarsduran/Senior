758959c590f92b00e0d2ec5126daed017743174d
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-06-11 23:54:25 +0300

    Added consumables

---
+++ Assets/Controller/Gathering/Consumables/Consumable.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace IdenticalStudios
+{
+    public class Consumable : Interactable
+    {
+        public event UnityAction<Consumable> Consumed;
+
+        [SerializeField, Range(-100f, 100f)]
+        [Tooltip("The minimum amount of hunger this consumable can restore.")]
+        private int m_HungerRestoreMin = 15;
+
+        [SerializeField, Range(-100f, 100f)]
+        [Tooltip("The maximum amount of hunger this consumable can restore.")]
+        private int m_HungerRestoreMax = 20;
+
+        [SerializeField, Range(-100f, 100f)]
+        [Tooltip("The minimum amount of thirst this consumable can restore.")]
+        private int m_ThirstRestoreMin = 5;
+
+        [SerializeField, Range(-100f, 100f)]
+        [Tooltip("The maximum amount of thirst this consumable can restore.")]
+        private int m_ThirstRestoreMax = 10;
+
+        [SerializeField]
+        [Tooltip("Audio that will be played after a character consumes this.")]
+        private SoundPlayer m_ConsumeAudio;
+
+
+        public override void OnInteract(ICharacter character)
+        {
+            base.OnInteract(character);
+
+            bool consumed = false;
+
+            if (character.TryGetModule(out IHungerManager hungerManager))
+            {              
+                hungerManager.Hunger += Random.Range(m_HungerRestoreMin, m_HungerRestoreMax);
+                consumed = true;
+            }
+
+            if (character.TryGetModule(out IThirstManager thirstManager))
+            {
+                thirstManager.Thirst += Random.Range(m_ThirstRestoreMin, m_ThirstRestoreMax);
+                consumed = true;
+            }
+
+            if (consumed)
+            {
+                m_ConsumeAudio.Play2D();
+
+                gameObject.SetActive(false);
+                Consumed?.Invoke(this);
+            }
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            Description = $"Hunger: +{m_HungerRestoreMin}-{m_HungerRestoreMax}" + "\n" + $"Thirst: +{m_ThirstRestoreMin}-{m_ThirstRestoreMax}";
+        }
+#endif
+    }
+}
---
+++ Assets/Controller/Gathering/Consumables/ConsumableManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace IdenticalStudios
+{
+    public class ConsumableManager : MonoBehaviour, ISaveableComponent
+    {
+        #region Internal
+        [Serializable]
+        public struct RespawnData 
+        {
+            [NonSerialized]
+            public Consumable Consumable;
+
+            public int ConsumableIndex;
+            public float RespawnTime;
+        }
+        #endregion
+
+        [SerializeField]
+        private bool m_RespawnConsumables;
+
+        [SerializeField]
+        private int m_RespawnTimeMin = 45;
+
+        [SerializeField]
+        private int m_RespawnTimeMax = 60;
+
+        private List<RespawnData> m_RespawnDatas;
+
+        private Consumable[] m_Consumables;
+
+
+        private void Awake()
+        {
+            m_Consumables = GetComponentsInChildren<Consumable>();
+            m_RespawnDatas = new List<RespawnData>(m_Consumables.Length);
+
+            foreach (var consumable in m_Consumables)
+                consumable.Consumed += OnItemConsumed;
+        }
+
+        private void Update()
+        {
+            int i = 0;
+            float deltaTime = Time.deltaTime;
+
+            while (i < m_RespawnDatas.Count)
+            {
+                var respawnData = m_RespawnDatas[i];
+                respawnData.RespawnTime -= deltaTime;
+
+                if (respawnData.RespawnTime <= 0f)
+                {
+                    respawnData.Consumable.gameObject.SetActive(true);
+                    m_RespawnDatas.Remove(respawnData);
+                }
+                else
+                    i++;
+            }
+        }
+
+        private void OnItemConsumed(Consumable consumable)
+        {
+            var newRespawnData = new RespawnData()
+            {
+                Consumable = consumable,
+                RespawnTime = Random.Range(m_RespawnTimeMin, m_RespawnTimeMax),
+                ConsumableIndex = GetIndexOfConsumable(consumable)
+            };
+
+            m_RespawnDatas.Add(newRespawnData);
+        }
+
+        private int GetIndexOfConsumable(Consumable consumable)
+        {
+            for (int i = 0; i < m_Consumables.Length; i++)
+            {
+                if (m_Consumables[i] == consumable)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        #region Save & Load
+        public void LoadMembers(object[] members)
+        {
+            m_RespawnDatas = members[0] as List<RespawnData>;
+
+            foreach (var data in m_RespawnDatas)
+                m_Consumables[data.ConsumableIndex].gameObject.SetActive(false);
+        }
+
+        public object[] SaveMembers()
+        {
+            return new object[] { m_RespawnDatas };
+        }
+        #endregion
+    }
+}
