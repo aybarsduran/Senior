f4b666bc7aa968310a1a8003234ef82cec8b98c8
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-06-11 23:56:16 +0300

    Added behaviours

---
+++ Assets/Controller/Gathering/Behaviours/TreeChopBehaviour.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+
+namespace IdenticalStudios.ResourceGathering
+{
+    public sealed class TreeChopBehaviour : GatherableBehaviour, ISaveableComponent
+    {
+        #region Internal
+        [System.Serializable]
+        private class ChoppingSegment
+        {
+            public bool Enabled
+            {
+                get => m_Enabled;
+                set
+                {
+                    m_Enabled = value;
+                    m_Object.SetActive(m_Enabled);
+                }
+            }
+
+            public GameObject Object 
+            {
+                get => m_Object;
+                set => m_Object = value;
+            }
+
+            public Vector3 Normal => m_Normal;
+
+            [SerializeField]
+            private bool m_Enabled;
+
+            private Vector3 m_Normal;
+            private GameObject m_Object;
+
+
+            public void Refresh() => m_Object.SetActive(m_Enabled);
+
+            public ChoppingSegment(bool enabled, GameObject obj, Vector3 normal)
+            {
+                m_Enabled = enabled;
+                m_Object = obj;
+                m_Normal = normal;
+            }
+        }
+        #endregion
+
+        [SerializeField]
+        [Tooltip("Center")]
+        private Transform m_ChoppingPivot;
+
+        [SerializeField]
+        private Transform m_ChoppingSegmentsRoot;
+
+        private ChoppingSegment[] m_ChoppingSegments;
+        private Vector3 m_SegmentsCenter;
+        private int m_ChoppedSegmentCount;
+
+
+        public override void DoHitEffects(DamageContext dmgInfo)
+        {
+            if (m_ChoppingSegments == null)
+            {
+                m_ChoppingSegmentsRoot.gameObject.SetActive(true);
+                m_ChoppingSegments = new ChoppingSegment[m_ChoppingSegmentsRoot.childCount];
+                PrepareChoppingSegments();
+            }
+
+            int disabledSegments = m_ChoppingSegments.Length - (int)((Gatherable.Health / 100f) * m_ChoppingSegments.Length);
+            int amountToChop = disabledSegments - m_ChoppedSegmentCount;
+
+            if (amountToChop > 0)
+            {
+                for (int i = 0; i < amountToChop; i++)
+                {
+                    int indexToSelect = 0;
+
+                    float largestAngle = 0f;
+                    Vector3 chopPointNormal = (dmgInfo.HitPoint - m_SegmentsCenter).normalized;
+
+                    for (int j = 0; j < m_ChoppingSegments.Length; j++)
+                    {
+                        if (!m_ChoppingSegments[j].Enabled)
+                            continue;
+
+                        float angle = Vector3.Angle(chopPointNormal, m_ChoppingSegments[j].Normal);
+
+                        if (angle > largestAngle)
+                        {
+                            largestAngle = angle;
+                            indexToSelect = j;
+                        }
+                    }
+
+                    m_ChoppingSegments[indexToSelect].Enabled = false;
+                }
+
+                m_ChoppedSegmentCount += amountToChop;
+            }
+        }
+
+        public override void DoDestroyEffects(DamageContext dmgInfo)
+        {
+            foreach (var segment in m_ChoppingSegments)
+                segment.Enabled = false;
+        }
+
+        private void Awake()
+        {
+            m_ChoppingSegmentsRoot.gameObject.SetActive(false);
+        }
+
+        private void PrepareChoppingSegments()
+        {
+            m_SegmentsCenter = m_ChoppingPivot.position;
+
+            int index = 0;
+
+            foreach (Transform segmentObject in m_ChoppingSegmentsRoot)
+            {
+                ChoppingSegment segment = m_ChoppingSegments[index];
+
+                var segmentNormal = (m_ChoppingPivot.position - segmentObject.position).normalized;
+
+                if (segment == null)
+                    segment = new ChoppingSegment(true, segmentObject.gameObject, segmentNormal);
+                else
+                {
+                    segment.Object = segmentObject.gameObject;
+                    segment.Refresh();
+                }
+
+                m_ChoppingSegments[index] = segment;
+
+                index++;
+            }
+        }
+
+        #region Save & Load
+        public void LoadMembers(object[] members)
+        {
+            m_ChoppedSegmentCount = (int)members[0];
+            m_ChoppingSegments = members[1] as ChoppingSegment[];
+
+            if (m_ChoppingSegments != null)
+                PrepareChoppingSegments();
+        }
+
+        public object[] SaveMembers()
+        {
+            return new object[] 
+            {
+                m_ChoppedSegmentCount,
+                m_ChoppingSegments           
+            };
+        }
+        #endregion
+    }
+}
---
+++ Assets/Controller/Gathering/Behaviours/TreeFallBehaviour.cs
@@ -0,0 +1,177 @@
+﻿using IdenticalStudios.ProceduralMotion;
+using System.Collections;
+using UnityEngine;
+
+namespace IdenticalStudios.ResourceGathering
+{
+    public sealed class TreeFallBehaviour : GatherableBehaviour, ISaveableComponent
+    {
+        [SerializeField]
+        private Rigidbody m_FallingTree;
+
+        [SerializeField]
+        private GameObject m_TreeStump;
+
+        [SerializeField]
+        private ColliderTriggerHandler m_ImpactTrigger;
+
+        [SerializeField]
+        private Rigidbody m_LogPrefab;
+
+        [SerializeField, Range(1, 100)]
+        private int m_LogsCount = 6;
+
+        [SerializeField, Range(0f, 100f)]
+        private float m_LogsOffset = 2f;
+
+
+        [SerializeField]
+        private GameObject m_TreeImpactFX;
+
+        [SerializeField]
+        private SoundPlayer m_TreeFallAudio;
+
+        [SerializeField]
+        private SoundPlayer m_TreeImpactAudio;
+
+        [SerializeField]
+        private ShakeSettings3D m_CameraShake = ShakeSettings3D.Default;
+
+        private const float k_MinTimeToTriggerImpact = 1f;
+        private const float k_MaxTimeToFall = 8f;
+        private const float k_LogsForce = 50f;
+
+        private bool m_IsFalling = false;
+        private float m_TimeSinceFallStart;
+        private Vector3 m_HitDirection = Vector3.zero;
+
+
+        public override void DoHitEffects(DamageContext damageInfo)
+        {
+            m_TreeStump.SetActive(true);
+            m_FallingTree.gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// Start tree fall
+        /// </summary>
+        /// <param name="dmgInfo"></param>
+        public override void DoDestroyEffects(DamageContext dmgInfo)
+        {
+            m_HitDirection = dmgInfo.HitForce;
+            StartTreeFall(m_HitDirection);
+        }
+
+        private void StartTreeFall(Vector3 hitForce) 
+        {
+            Vector3 force = new(hitForce.x, 0, hitForce.z);
+
+            m_FallingTree.GetComponent<Collider>().enabled = true;
+            m_FallingTree.isKinematic = false;
+            m_FallingTree.AddForce(force, ForceMode.Impulse);
+
+            m_ImpactTrigger.TriggerEnter += OnTreeImpact;
+
+            m_TreeFallAudio.PlayAtPosition(transform.position, 1f, SelectionType.Random);
+            m_TreeStump.SetActive(true);
+
+            m_IsFalling = true;
+
+            UpdateManager.AddUpdate(UpdateFall);
+        }
+
+        private void Awake() => ResetTree();
+
+        private void OnDestroy()
+        {
+            if (m_IsFalling)
+                UpdateManager.RemoveUpdate(UpdateFall);
+        }
+
+        private void ResetTree()
+        {
+            m_FallingTree.GetComponent<Collider>().enabled = false;
+            m_FallingTree.isKinematic = true;
+            m_FallingTree.gameObject.SetActive(false);
+
+            m_TreeStump.SetActive(false);
+            m_TimeSinceFallStart = 0f;
+        }
+
+        private void UpdateFall(float deltaTime)
+        {
+            // Force start the tree impact behaviour when the time limit is up or the velocity of the tree fall is close to 0.
+            if (m_TimeSinceFallStart > k_MinTimeToTriggerImpact)
+            {
+                if (m_TimeSinceFallStart > k_MaxTimeToFall || m_FallingTree.angularVelocity.sqrMagnitude < 0.005f)
+                    OnTreeImpact(null);
+            }
+
+            m_TimeSinceFallStart += deltaTime;
+        }
+
+        private void OnTreeImpact(Collider other)
+        {
+            m_IsFalling = false;
+
+            UpdateManager.RemoveUpdate(UpdateFall);
+            m_ImpactTrigger.TriggerEnter -= OnTreeImpact;
+
+            StartCoroutine(C_DelayedImpact());
+        }
+
+        private IEnumerator C_DelayedImpact() 
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            m_FallingTree.GetComponent<Collider>().enabled = false;
+            var fallingTree = m_FallingTree.transform;
+
+            for (int i = 0; i < m_LogsCount; i++)
+            {
+                var log = Instantiate(m_LogPrefab, fallingTree);
+                log.transform.SetParent(fallingTree, false);
+                log.transform.localPosition = i * m_LogsOffset * Vector3.up;
+                log.AddForce(k_LogsForce * Mathf.Sign(Random.Range(-100f, 100f)) * log.transform.right, ForceMode.Impulse);
+
+                if (m_TreeImpactFX != null)
+                    Instantiate(m_TreeImpactFX, log.position, log.rotation);
+
+                log.transform.SetParent(null, true);
+            }
+
+            yield return null;
+            m_FallingTree.GetComponent<Collider>().enabled = true;
+            m_FallingTree.gameObject.SetActive(false);
+
+            CameraShakeManagerBase.DoShake(transform.position, m_CameraShake, 1f);
+            m_TreeImpactAudio.PlayAtPosition(transform.position, 1f, SelectionType.RandomExcludeLast);
+        }
+
+        #region Save & Load
+        public void LoadMembers(object[] members)
+        {
+            m_IsFalling = (bool)members[0];
+            m_HitDirection = (Vector3)members[1];
+
+            if (m_IsFalling)
+            {
+                StartTreeFall(m_HitDirection);
+                print(m_IsFalling);
+            }
+
+            m_TreeStump.SetActive((bool)members[2]);
+        }
+
+        public object[] SaveMembers()
+        {
+            return new object[]
+            {
+                m_IsFalling,
+                m_HitDirection,
+                m_TreeStump.activeSelf
+            };
+        }
+        #endregion
+    }
+}
